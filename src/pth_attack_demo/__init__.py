89bb3a1ccaee250ca1b0d6ab0c8568726197c665
Noai-oss <jiuwoxiao@outlook.com>
2026-05-31 12:33:58 +0800

    fix: 🐛 fix package name with `_` rather than `-`

--- src/pth_attack_demo/__init__.py
+++ src/pth_attack_demo/__init__.py
@@ -1,6 +1,6 @@
 from importlib.metadata import version, PackageNotFoundError
 
 try:
-    __version__ = version("pth-attack-demo")
+    __version__ = version("pth_attack_demo")
 except PackageNotFoundError:
     __version__ = "0.0.0"
